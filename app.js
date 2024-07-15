5de1904273d400e7bd3aeacd9cca3fcadc0fee48
Ravindu9EV <yravindu18@gmail.com>
2024-07-16 01:50:36 +0530

    add js and style

---
+++ app.js
@@ -0,0 +1,11 @@
+//let pokemon="https://api.pokemontcg.io/v2/cards"
+
+function showBar() {
+  const sidebar = document.querySelector(".sidebar");
+  sidebar.style.display = "flex";
+}
+
+function hideBar() {
+  let sidebar = document.querySelector(".sidebar");
+  sidebar.style.display = "none";
+}
